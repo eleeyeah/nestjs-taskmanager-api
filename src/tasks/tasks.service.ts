7974b1373ef978b25750758341cf8914a2eac16b
eleeyeah <ilietmks@gmail.com>
2023-07-28 18:59:54 +0200

    error handling for getting and deleting inexistent tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,5 @@
 import { Task, TaskStatus } from './task.model';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import * as uuid from 'uuid'; // this is a library that will generate a unique id for us
 import { createTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
@@ -49,7 +49,13 @@ export class TasksService {
 
   // GET TASK BY ID
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id); // find the task with the same id as the id we passed in
+    const found = this.tasks.find((task) => task.id === id); // find the task with the same id as the id we passed in
+
+    // Error Handling
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+    return found;
   }
 
   // CREATE A TASK
@@ -71,7 +77,9 @@ export class TasksService {
 
   // DELETE A TASK
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id); // filter out the task with the same id as the id we passed in
+    // Error Handling
+    const found = this.getTaskById(id); // Error Handling
+    this.tasks = this.tasks.filter((task) => task.id !== found.id); // filter out the task with the same id as the id we passed in
   }
 
   // UPDATE A TASK
